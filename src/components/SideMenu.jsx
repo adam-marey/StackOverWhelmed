bbf2bc42d8fff08ac3875215a8bdee60ea34dd6f
adam <adammarey51@gmail.com>
2023-12-08 21:05:15 -0700

    initilized the app - css

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import './SideMenu.css';
+
+function SideMenu() {
+  return <div className="side-menu" style={{ height: '100vh' }} />;
+}
+
+export default SideMenu;
