2a4735e642b916b079c1bfbd6a673328c8143d62
adam <adammarey51@gmail.com>
2024-05-04 14:43:49 -0700

    code highlight tested

--- src/components/Topics/ArraysPage.jsx
+++ src/components/Topics/ArraysPage.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import CodeSnippet from '../CodeSnippet';
 
-const ArraysPage = () => {
+const ArrayPage = () => {
+  const javascriptCode = `const greeting = "Hello, world!";`;
+  const pythonCode = `print("Hello, world!")`;
+  const CCode = `#include <stdio.h>\n\nint main() {\n    printf("%s", "Hello, world!");\n    return 0;\n}`;
+
   return (
-    <div className="home">
-      <p>This is the page for practicing array-related problems.</p>
+    <div>
+      <h2>JavaScript Code</h2>
+      <CodeSnippet code={javascriptCode} language="javascript" />
+
+      <h2>Python Code</h2>
+      <CodeSnippet code={pythonCode} language="python" />
+
+      <h2>C# code</h2>
+      <CodeSnippet code={CCode} language="C++" />
     </div>
   );
 };
 
-export default ArraysPage;
+export default ArrayPage;
